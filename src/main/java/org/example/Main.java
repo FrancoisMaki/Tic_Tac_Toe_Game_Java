f50659aab82709d9f76fd209d0ffb25a1ab86eae
FrancoisMaki <francoismakijess@gmail.com>
2025-06-19 16:02:11 +0200

    add the principal game loop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,71 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        game();
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public static void game(){
+        char [][] base = new char[3][3];
+        boolean p1Turn;
+        System.out.println("Game Start");
+        printArrray(base);
+        do {
+            System.out.println("1st Player Turn");
+            p1Turn=true;
+            insert(base, p1Turn);
+            printArrray(base);
+
+            System.out.println("--------------------------------------------------\n\n");
+
+            System.out.println("2nd Player Turn");
+            p1Turn = false;
+            insert(base, p1Turn);
+            printArrray(base);
+
+            System.out.println("--------------------------------------------------\n\n");
+        }while (1!=0);
+    }
+
+    private static void insert(char[][] base, boolean p1Turn){
+        if (p1Turn){
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Select the position to mark");
+
+            System.out.println("Select the row");
+            int row = sc.nextInt();
+
+            System.out.println("Select the column");
+            int column = sc.nextInt();
+
+            base[row-1][column-1] = 'X';
+        }else{
+            Scanner sc = new Scanner(System.in);
+
+            System.out.println("Select the position to mark");
+
+            System.out.println("Select the row");
+            int row = sc.nextInt();
+
+            System.out.println("Select the column");
+            int column = sc.nextInt();
+
+            base[row-1][column-1] = 'O';
+        }
+    }
+
+
+
+    private static void printArrray(char[][] base){
+        for (char[] chars : base) {
+            for (char aChar : chars) {
+                System.out.print("|" + aChar + "|");
+            }
+            System.out.println();
         }
     }
 }
